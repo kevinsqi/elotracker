be1b867e448f16bf1cd258c624364e56804b3265
Kevin Qi <iqnivek@gmail.com>
2012-06-28 10:34:52 +0800

    Initialized rails 3.2.6 app with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsScaffold::Application.config.secret_token = '620902791f8706a21e274602a2918394cc53294e1265a603078200aec2ce0c36831957d2216df49f1f0c32639645d65d695ba1cb2af13656573089467fca9f76'
