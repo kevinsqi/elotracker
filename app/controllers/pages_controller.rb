ecd97c3c78ca85d6bf3efa18bcd9c87666bf6d47
Kevin Qi <iqnivek@gmail.com>
2012-07-17 01:31:08 -0400

    Add basic league creation form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def index
     @page_title = 'Custom ELO tracker for any competition'
+    @league = League.new
+    5.times do  # TODO named constant
+      @league.players.build
+    end
   end
 end
