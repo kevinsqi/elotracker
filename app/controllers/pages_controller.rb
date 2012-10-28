ab4413cbe4728299528104707cdf695558f19ecf
Kevin Qi <iqnivek@gmail.com>
2012-10-27 22:51:40 -0400

    Remove tagline, use placeholder on input

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @page_title = 'Custom competition ELO tracker'
-    @league = League.new(:name => '2012 Boston Foosball League')  # TODO generate names
+    @page_title = 'Track anything with ELO'
+    @league = League.new
   end
 end
